8f00bf17756e1e490d5014978993d9a305796f54
Mark Saddler <msaddler@mit.edu>
2025-04-11 16:33:09 +0200

    modified util.make_nervegram_plot from phaselocknet.util_figures.make_stimulus_summary_plot

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker
 import numpy as np
 
 
@@ -30,13 +31,6 @@ def set_dbspl(x, dbspl, mean_subtract=True):
         x = x - np.mean(x)
     rms_out = 20e-6 * np.power(10, dbspl / 20)
     return rms_out * x / rms(x)
-
-
-def loguniform(low, high, size=None):
-    """
-    Draw random samples uniformly on a log scale.
-    """
-    return np.exp(np.random.uniform(low=np.log(low), high=np.log(high), size=size))
 
 
 def logspace(start, stop, num):
@@ -325,3 +319,246 @@ def make_spectrogram_plot(
     kwargs_format_axes.update(kwargs)
     ax_arr[1] = format_axes(ax_arr[1], **kwargs_format_axes)
     return fig, ax_arr
+
+
+def plot_nervegram(
+    ax,
+    nervegram,
+    sr=20000,
+    cfs=None,
+    cmap="gray",
+    cbar_on=False,
+    fontsize_labels=12,
+    fontsize_ticks=12,
+    fontweight_labels=None,
+    nxticks=6,
+    nyticks=5,
+    tmin=None,
+    tmax=None,
+    treset=True,
+    vmin=None,
+    vmax=None,
+    interpolation="none",
+    vticks=None,
+    str_clabel=None,
+    **kwargs_format_axes,
+):
+    """
+    Helper function to visualize simulated auditory nerve representation.
+    """
+    # Trim nervegram if tmin and tmax are specified
+    nervegram = np.squeeze(nervegram)
+    assert len(nervegram.shape) == 2, "nervegram must be freq-by-time array"
+    t = np.arange(0, nervegram.shape[1]) / sr
+    if (tmin is not None) and (tmax is not None):
+        t_IDX = np.logical_and(t >= tmin, t < tmax)
+        t = t[t_IDX]
+        nervegram = nervegram[:, t_IDX]
+    if treset:
+        t = t - t[0]
+    # Setup time and frequency ticks and labels
+    time_idx = np.linspace(0, t.shape[0] - 1, nxticks, dtype=int)
+    time_labels = ["{:.0f}".format(1e3 * t[itr0]) for itr0 in time_idx]
+    if cfs is None:
+        cfs = np.arange(0, nervegram.shape[0])
+    else:
+        cfs = np.array(cfs)
+        msg = f"{cfs.shape[0]=} must match {nervegram.shape[0]=}"
+        assert cfs.shape[0] == nervegram.shape[0], msg
+    freq_idx = np.linspace(0, cfs.shape[0] - 1, nyticks, dtype=int)
+    freq_labels = ["{:.0f}".format(cfs[itr0]) for itr0 in freq_idx]
+    # Display nervegram image
+    im_nervegram = ax.imshow(
+        nervegram,
+        origin="lower",
+        aspect="auto",
+        extent=[0, nervegram.shape[1], 0, nervegram.shape[0]],
+        cmap=cmap,
+        vmin=vmin,
+        vmax=vmax,
+        interpolation=interpolation,
+    )
+    # Add colorbar if `cbar_on == True`
+    if cbar_on:
+        cbar = plt.colorbar(im_nervegram, ax=ax, pad=0.02)
+        cbar.ax.set_ylabel(
+            str_clabel, fontsize=fontsize_labels, fontweight=fontweight_labels
+        )
+        if vticks is not None:
+            cbar.set_ticks(vticks)
+        else:
+            cbar.ax.yaxis.set_major_locator(
+                matplotlib.ticker.MaxNLocator(nyticks, integer=True)
+            )
+        cbar.ax.tick_params(
+            direction="out",
+            axis="both",
+            which="both",
+            labelsize=fontsize_ticks,
+            length=fontsize_ticks / 2,
+        )
+        cbar.ax.yaxis.set_major_formatter(matplotlib.ticker.FormatStrFormatter("%03d"))
+    # Format axes
+    ax = format_axes(
+        ax,
+        xticks=time_idx,
+        yticks=freq_idx,
+        xticklabels=time_labels,
+        yticklabels=freq_labels,
+        fontsize_labels=fontsize_labels,
+        fontsize_ticks=fontsize_ticks,
+        fontweight_labels=fontweight_labels,
+        **kwargs_format_axes,
+    )
+    return ax
+
+
+def make_nervegram_plot(
+    ax_arr,
+    ax_idx_waveform=None,
+    ax_idx_spectrum=None,
+    ax_idx_nervegram=None,
+    ax_idx_excitation=None,
+    waveform=None,
+    nervegram=None,
+    sr_waveform=None,
+    sr_nervegram=None,
+    cfs=None,
+    tmin=None,
+    tmax=None,
+    treset=True,
+    vmin=None,
+    vmax=None,
+    interpolation="none",
+    erb_freq_axis=True,
+    nxticks=6,
+    nyticks=6,
+    kwargs_plot={"color": "k", "lw": 1},
+    limits_buffer=0.1,
+    **kwargs_format_axes,
+):
+    """
+    Generate figure with auditory nerve representation flanked
+    by stimulus waveform, power spectrum, and excitation pattern.
+    """
+    # Axes are tracked in flattened array
+    ax_arr = np.array([ax_arr]).reshape([-1])
+    ax_idx_list = []
+
+    # Plot stimulus waveform
+    if ax_idx_waveform is not None:
+        ax_idx_list.append(ax_idx_waveform)
+        y_wav = np.squeeze(waveform)
+        assert len(y_wav.shape) == 1, "waveform must be 1D array"
+        x_wav = np.arange(0, y_wav.shape[0]) / sr_waveform
+        if (tmin is not None) and (tmax is not None):
+            IDX = np.logical_and(x_wav >= tmin, x_wav < tmax)
+            x_wav = x_wav[IDX]
+            y_wav = y_wav[IDX]
+        if treset:
+            x_wav = x_wav - x_wav[0]
+        xlimits_wav = [x_wav[0], x_wav[-1]]
+        ylimits_wav = [-np.max(np.abs(y_wav)), np.max(np.abs(y_wav))]
+        ylimits_wav = np.array(ylimits_wav) * (1 + limits_buffer)
+        ax_arr[ax_idx_waveform].plot(x_wav, y_wav, **kwargs_plot)
+        ax_arr[ax_idx_waveform] = format_axes(
+            ax_arr[ax_idx_waveform],
+            xlimits=xlimits_wav,
+            ylimits=ylimits_wav,
+            xticks=[],
+            yticks=[],
+            xticklabels=[],
+            yticklabels=[],
+            **kwargs_format_axes,
+        )
+
+    # Plot stimulus power spectrum
+    if ax_idx_spectrum is not None:
+        ax_idx_list.append(ax_idx_spectrum)
+        fxx, pxx = power_spectrum(waveform, sr_waveform)
+        if cfs is not None:
+            msg = "Frequency axes will not align when highest CF exceeds Nyquist"
+            assert np.max(cfs) <= np.max(fxx), msg
+            IDX = np.logical_and(fxx >= np.min(cfs), fxx <= np.max(cfs))
+            pxx = pxx[IDX]
+            fxx = fxx[IDX]
+        xlimits_pxx = [np.max(pxx) * (1 + limits_buffer), 0]  # Reverses x-axis
+        xlimits_pxx = np.ceil(np.array(xlimits_pxx) * 5) / 5
+        if erb_freq_axis:
+            fxx = freq2erb(fxx)
+            ylimits_fxx = [np.min(fxx), np.max(fxx)]
+            yticks = np.linspace(ylimits_fxx[0], ylimits_fxx[-1], nyticks)
+            yticklabels = ["{:.0f}".format(yt) for yt in erb2freq(yticks)]
+        else:
+            ylimits_fxx = [np.min(fxx), np.max(fxx)]
+            yticks = np.linspace(ylimits_fxx[0], ylimits_fxx[-1], nyticks)
+            yticklabels = ["{:.0f}".format(yt) for yt in yticks]
+        ax_arr[ax_idx_spectrum].plot(pxx, fxx, **kwargs_plot)
+        ax_arr[ax_idx_spectrum] = format_axes(
+            ax_arr[ax_idx_spectrum],
+            str_xlabel="Power\n(dB SPL)",
+            str_ylabel="Frequency (Hz)",
+            xlimits=xlimits_pxx,
+            ylimits=ylimits_fxx,
+            xticks=xlimits_pxx,
+            yticks=yticks,
+            xticklabels=xlimits_pxx.astype(int),
+            yticklabels=yticklabels,
+            **kwargs_format_axes,
+        )
+
+    # Plot stimulus nervegram
+    if ax_idx_nervegram is not None:
+        ax_idx_list.append(ax_idx_nervegram)
+        if ax_idx_spectrum is not None:
+            nervegram_nxticks = nxticks
+            nervegram_nyticks = 0
+            nervegram_str_xlabel = "Time\n(ms)"
+            nervegram_str_ylabel = None
+        else:
+            nervegram_nxticks = nxticks
+            nervegram_nyticks = nyticks
+            nervegram_str_xlabel = "Time (ms)"
+            nervegram_str_ylabel = "Characteristic frequency (Hz)"
+        plot_nervegram(
+            ax_arr[ax_idx_nervegram],
+            nervegram,
+            sr=sr_nervegram,
+            cfs=cfs,
+            nxticks=nervegram_nxticks,
+            nyticks=nervegram_nyticks,
+            tmin=tmin,
+            tmax=tmax,
+            treset=treset,
+            vmin=vmin,
+            vmax=vmax,
+            interpolation=interpolation,
+            str_xlabel=nervegram_str_xlabel,
+            str_ylabel=nervegram_str_ylabel,
+        )
+
+    # Plot time-averaged excitation pattern
+    if ax_idx_excitation is not None:
+        ax_idx_list.append(ax_idx_excitation)
+        x_exc = np.mean(nervegram, axis=1)
+        xlimits_exc = np.array([0, np.max(x_exc) * (1 + limits_buffer)])
+        y_exc = np.arange(0, nervegram.shape[0])
+        ylimits_exc = [np.min(y_exc), np.max(y_exc)]
+        ax_arr[ax_idx_excitation].plot(x_exc, y_exc, **kwargs_plot)
+        ax_arr[ax_idx_excitation] = format_axes(
+            ax_arr[ax_idx_excitation],
+            str_xlabel="Excitation\npattern",
+            xlimits=xlimits_exc,
+            ylimits=ylimits_exc,
+            xticks=[],
+            yticks=[],
+            xticklabels=[],
+            yticklabels=[],
+            **kwargs_format_axes,
+        )
+
+    # Clear unused axes in ax_arr
+    for ax_idx in range(ax_arr.shape[0]):
+        if ax_idx not in ax_idx_list:
+            ax_arr[ax_idx].axis("off")
+    return ax_arr
